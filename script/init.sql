6230848e7f9fac4966c71af4a07f67e38963c7ad
pupiesa <puppup.2547.2016@gmail.com>
2025-06-26 23:41:26 +0700

    Update MySQL config and add license_plates table
    Set a custom container name and database for MySQL in docker-compose, change exposed port to 3307, and update the initialization SQL to create the 'licence-plate' database and a 'license_plates' table for storing plate data.

--- script/init.sql
+++ script/init.sql
@@ -1,3 +1,13 @@
 CREATE USER IF NOT EXISTS 'copter50029'@'%' IDENTIFIED BY 'copter12345';
 GRANT ALL PRIVILEGES ON *.* TO 'copter50029'@'%';
 FLUSH PRIVILEGES;
+
+USE `licence-plate`;
+
+CREATE TABLE IF NOT EXISTS `license_plates` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `plate_id` VARCHAR(15) NOT NULL,
+  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `image_url` TEXT NOT NULL,
+  `camera_id` VARCHAR(10) NOT NULL
+);
